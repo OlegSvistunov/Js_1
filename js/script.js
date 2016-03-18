63be2a5e4b53bae0810c96edab18aac217c7f71c
OlegSV80 <svistunov.oleg80@gmail.com>
2016-03-18 23:16:39 +0200

    JS_1
    Up JS_1

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function pow(a, b){
+	 number = prompt('Введите, пожалуйста, число' , 0);
+	 degree = prompt('Введите, пожалуйста, степень' , 0);
+	var a = parseInt(number);
+	var b = parseInt(degree);
+  var result;
+    switch (true){
+                  case (b == 0): // Если степень равна 0
+                      result = 1;
+                      // console.log('число в степени = ', result);
+                      break;
+                  case (b == 1): // Если степень равна 1
+                      result = a;
+                      // console.log('число в степени = ', result);
+                      break;
+                  case (b < 0): // Если степень меньше 0
+                      result = a;
+                      for (var j = 1; j < (b * (-1)); j++) {
+                          result = result * a;
+                          // console.log('число в степени = ', result);
+                      }
+                      result = 1 / result;
+                      // console.log('число в степени = ', result);
+                      break;
+                  default: // Если степень больше 1
+                      result = a;
+                      for (var i = 1; i < b; i++) {
+                          result = result * a;
+                      }
+                      // console.log('число в степени = ', result);
+              }
+    return result;
+}
+var degreeResult = pow();
+console.log('число ', number, ' в степени ', degree, ' = ', degreeResult); // Вывод результата в консоль
+document.write('число ', number, ' в степени ', degree, ' = ', degreeResult); // Вывод результата на экран
